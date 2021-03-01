eaf2cbce3fa88ffd5efe12f627b0018e006d7b24
Akito Nakamura <akito.nakamura.c@gmail.com>
2021-03-01 18:07:37 +0900

    ログイン機能完成

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const { checkAuthenticated } = require("../config/auth");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", checkAuthenticated, function (req, res, next) {
+  res.render("index", { title: "index" });
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,70 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const bcrypt = require("bcrypt");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const { checkNotAuthenticated } = require("../config/auth");
+
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: "username",
+      passwordField: "password",
+    },
+    function (username, password, done) {
+      if (username === "akito" && password === "1111") {
+        return done(null, username);
+      } else {
+        return done(null, false);
+      }
+    }
+  )
+);
+
+// session
+passport.serializeUser(function (user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function (user, done) {
+  done(null, user);
+});
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+// register
+router.get("/register", checkNotAuthenticated, function (req, res, next) {
+  res.render("register", { title: "register" });
+});
+
+router.post("/register", async function (req, res, next) {
+  const { username, password } = req.body;
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    res.redirect("/users/login");
+  } catch {
+    res.redirect("/users/register");
+  }
+});
+
+// login
+router.get("/login", checkNotAuthenticated, function (req, res, next) {
+  res.render("login", { title: "login" });
+});
+
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/users/login",
+    failureFlash: true,
+  })
+);
+
+// logout
+router.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect("/users/login");
 });
 
 module.exports = router;
